96a4b157959f718ab53f597908a5d3c13e921224
Sergio Regueiro Martinez <sergioregueiro12@gmail.com>
2026-09-20 09:41:28 +0200

    Primer Ejercicio

---
+++ TwoSums/Main.java
@@ -0,0 +1,36 @@
+package org.LeetCode.TwoSums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i,j = 0;
+        for ( i = 0; i < nums.length; i++) {
+            for ( j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
+
+class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
